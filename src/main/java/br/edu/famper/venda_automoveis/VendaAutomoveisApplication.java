48c4e758bbf1d7210c65a79f3f3a6da3cb836a98
Eduardo <dusotoriva@gmail.com>
2024-09-25 20:08:46 -0300

    Initial commit

---
+++ src/main/java/br/edu/famper/venda_automoveis/VendaAutomoveisApplication.java
@@ -0,0 +1,13 @@
+package br.edu.famper.venda_automoveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaAutomoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaAutomoveisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/famper/venda_automoveis/VendaAutomoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.famper.venda_automoveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendaAutomoveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
